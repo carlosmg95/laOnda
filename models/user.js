d78cd565bef3595cd982320f4d466ea41d88f267
Carlos Moro García <carlosmoro95@gmail.com>
2016-06-15 15:58:32 +0200

    Migrations y Seeders

--- models/user.js
+++ models/user.js
@@ -25,7 +25,7 @@ module.exports = function(sequelize, DataTypes) {
 				defaultValue: false
 			},
 			cargo: {
-				DataTypes.ENUM('Presidentx', 'Vicepresidentx', 'Secretarix', 'Tesorerx')
+				type: DataTypes.ENUM('Presidentx', 'Vicepresidentx', 'Secretarix', 'Tesorerx')
 			}
 		},
 		{	instanceMethods: {
